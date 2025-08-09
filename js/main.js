c2f95237b3f13ca7cacd2e0a47a2752685f7e125
MAXKAMOVILHOMJON <maxkamovi538@gmail.com>
2025-08-09 10:29:34 +0500

    masala

---
+++ js/main.js
@@ -0,0 +1,104 @@
+// 1masala
+
+
+let a = 40 ;
+let b = 30 ;
+let s = a * b ;
+let pe = 2 * (a + b)
+console.log(" " + s + "  " + pe);
+
+// 2masala
+let d = 8
+let p = 3.14
+let r = d / 2
+s = p + r**2
+console.log( r  +" " + s );
+
+// 3masala
+a = 5;
+let v = a**3;
+s = 6 * a**2
+console.log( v + "  " + s  );
+
+// 4masala
+
+let c = 4
+a = 4
+b = 4
+ v = a * b * c
+ s = 2 * (a * b + b * c + a * c)
+ console.log( v + " " + s);
+
+//  5masala
+
+a = 12
+b = 21
+let m = (a + b)/2
+console.log( m );
+
+ //  7masala
+let r1 = 7
+let r2 = 4
+let S1 = p * r1
+let S2 = p * r2
+
+let S = p * (r1**2 - r2**2 ) ; 
+console.log(S1+"  "+S2+" "+S);
+
+//  8masala
+r = 4
+let L = 2 * p * r 
+S = p * r**2
+
+console.log(L + "   "+ S );
+
+let A = 8
+
+let B = 6
+
+let C = 2
+   A = A - C 
+   B = A + C -B 
+   C = A + B   
+ console.log(A+"  "+ B + "  " + C  );
+
+//  11masala
+
+
+   let x = 2
+  let y = 4 * (x - 3 )**6 - 7 * (x - 3)**3 + 2    
+
+ console.log(y);
+ //  12masala
+
+ let X = 3 
+ A = 5
+
+y = A**X
+
+console.log( y );
+//  13masala
+
+let Tf =40 
+let Tc = (Tf - 32) * 5/9
+console.log(Tc );
+
+//  14masala
+X = 20
+let Y =20
+A = 32000
+B = 35000
+let somx = A / X 
+let somy = B / Y
+
+let  som = somy - somx
+console.log(somx + "  " + somy + "  " + som );
+
+//  15masala
+som = 6000
+X = 2
+Y = 3
+A = X * som 
+B = Y * som
+let J = A + B 
+console.log(A +"  "+ B +"  "+ J);
